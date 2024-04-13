1adf3f74052e044a8762d209a1177ab2d2ac4c03
ValennPK <valentincanziani20@gmail.com>
2024-04-13 16:14:10 -0300

    basic index and com

--- api/server.go
+++ api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	db "github.com/ValennPK/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -18,15 +20,23 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	//fmt.Println("Ruta para cargar archivos HTML:", "static/*")
+	router.LoadHTMLGlob("static/templates/*")
+
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	router.POST("/users", server.createUser)
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.POST("/transfers", server.createTransfer)
-
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
 	//add routes to router
 	server.router = router
 	return server
